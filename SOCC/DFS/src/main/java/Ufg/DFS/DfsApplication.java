fa61948fd6c11d70377147134c18c7c8bf21461e
venilsongomes <49803103+venilsongomes@users.noreply.github.com>
2025-05-02 22:14:34 -0300

    Adição Projeto Inicial
    Adição Projeto Inicial com as Dependências necessárias

---
+++ SOCC/DFS/src/main/java/Ufg/DFS/DfsApplication.java
@@ -0,0 +1,13 @@
+package Ufg.DFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfsApplication.class, args);
+	}
+
+}

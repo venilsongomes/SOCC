f5fb4ffbc322264977db612050bdebe81338f6d3
Reydner Miranda <92489721+Reysco@users.noreply.github.com>
2025-06-04 11:47:42 -0300

    Add files via upload

---
+++ SOCC/DFS/src/main/java/Ufg/DFS/Repository/DocenteRepository.java
@@ -0,0 +1,9 @@
+package Ufg.DFS.Repository;
+
+import Ufg.DFS.Model.Docente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocenteRepository extends JpaRepository<Docente, Integer> {
+}

68d819f92b79dc9ef2f1de1a5ca80800b91ed46d
Reydner Miranda <92489721+Reysco@users.noreply.github.com>
2025-06-04 11:50:31 -0300

    Update DocenteController.java

--- SOCC/DFS/src/main/java/Ufg/DFS/Controller/DocenteController.java
+++ SOCC/DFS/src/main/java/Ufg/DFS/Controller/DocenteController.java
@@ -1,5 +1,59 @@
 package Ufg.DFS.Controller;
 
+import Ufg.DFS.Model.Docente;
+import Ufg.DFS.Service.DocenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/docentes")
+public class DocenteController {
+
+    @Autowired
+    private DocenteService docenteService;
+
+    @GetMapping
+    public ResponseEntity<List<Docente>> listarTodosDocentes() {
+        return ResponseEntity.ok(docenteService.getAllDocentes());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Docente> buscarDocentePorId(@PathVariable Integer id) {
+        return docenteService.getDocenteById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Docente> criarDocente(@RequestBody Docente docente) {
+        Docente novoDocente = docenteService.createDocente(docente);
+        return ResponseEntity.ok(novoDocente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Docente> atualizarDocente(@PathVariable Integer id, @RequestBody Docente docenteDetails) {
+        try {
+            Docente atualizado = docenteService.updateDocente(id, docenteDetails);
+            return ResponseEntity.ok(atualizado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarDocente(@PathVariable Integer id) {
+        docenteService.deleteDocente(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
+
+
+/*package Ufg.DFS.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,3 +75,4 @@ public class DocenteController {
     @DeleteMapping("/{id}")
     public void deletarDocente(@PathVariable Integer id) {}
 }
+*/
